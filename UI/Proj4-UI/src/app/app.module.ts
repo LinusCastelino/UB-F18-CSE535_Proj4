ec03bca4562b4f851a7450351ab7690e7aa253d0
Linus Castelino <castelinolinus@gmail.com>
2018-11-23 02:14:00 -0500

    created searchbar component and added to home

--- UI/Proj4-UI/src/app/app.module.ts
+++ UI/Proj4-UI/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +15,7 @@ import { SearchStatisticsComponent } from './search-statistics/search-statistics
 import { ToggleComponent } from './toggle/toggle.component';  
 
 import { APICallsService } from '../services/apicalls.service';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 const routes : Routes= [
   {path:'', redirectTo:'/home', pathMatch:'full'},
@@ -24,13 +29,18 @@ const routes : Routes= [
     HomeComponent,
     SearchResultsComponent,
     SearchStatisticsComponent,
-    ToggleComponent
+    ToggleComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [APICallsService],
   bootstrap: [AppComponent]
