e85e4340a04fd1f4320e768e07335599f0c92bda
Linus Castelino <castelinolinus@gmail.com>
2018-11-24 19:06:10 -0500

    Handled CORs error in API layer

--- API/src/main/java/com/ir/proj4/model/QueryController.java
+++ API/src/main/java/com/ir/proj4/model/QueryController.java
@@ -6,6 +6,7 @@ import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ public class QueryController<JSONObject> {
 	@Autowired
 	private SolrService solrService;
 	
+	@CrossOrigin
 	@RequestMapping(value="ir", method = RequestMethod.GET)
 	public ArrayList<Docs> redirectToExternalUrl1(@RequestParam("q") String query) throws URISyntaxException, GeneralSecurityException, IOException {
 //		System.out.println("Inside");
