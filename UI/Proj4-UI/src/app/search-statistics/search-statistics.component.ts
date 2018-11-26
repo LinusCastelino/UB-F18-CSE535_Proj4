64b9d5150b12a78b9586c13465766eb81f833334
Linus Castelino <castelinolinus@gmail.com>
2018-11-26 02:47:19 -0500

    Added google geochart with default configs

--- UI/Proj4-UI/src/app/search-statistics/search-statistics.component.ts
+++ UI/Proj4-UI/src/app/search-statistics/search-statistics.component.ts
@@ -10,6 +10,21 @@ export class SearchStatisticsComponent implements OnInit {
 
   constructor(private apiService: APICallsService) { }
 
+  map_data = [
+    ['Germany', 200],
+    ['US', 300],
+    ['Brazil', 400],
+    ['Canada', 500],
+    ['France', 600],
+    ['RU', 700]
+  ];
+  map_options = {
+    colorAxis: {colors: ['#F9A6A6', '#FF0000']},
+    backgroundColor: '#9BC9FD',
+    datalessRegionColor: '#EBE9E9',
+    defaultColor: '#f5f5f5',
+  };
+  
   ngOnInit() {
   }
 
