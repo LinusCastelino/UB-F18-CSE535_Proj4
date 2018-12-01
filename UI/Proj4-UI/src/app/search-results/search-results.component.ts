00f1131b820893fb01274d9ae24412089388e88b
Anand Gangar <agangar@buffalo.edu>
2018-12-01 00:03:43 -0500

    adding topics and verified filter

--- UI/Proj4-UI/src/app/search-results/search-results.component.ts
+++ UI/Proj4-UI/src/app/search-results/search-results.component.ts
@@ -1,46 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { APICallsService } from '../../services/apicalls.service'
-import { PageEvent } from '@angular/material';
-import {FormControl} from '@angular/forms';
-import {MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import { PageEvent, getMatTooltipInvalidPositionError } from '@angular/material';
+import {MatDatepickerInputEvent} from '@angular/material/datepicker';
+import { FormControl } from '@angular/forms';
 
-// Depending on whether rollup is used, moment needs to be imported differently.
-// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
-// syntax. However, rollup creates a synthetic default module and we thus need to import it using
-// the `default as` syntax.
-import * as _moment from 'moment';
-// tslint:disable-next-line:no-duplicate-imports
-//import {MomentBuiltinFormat as _rollupMoment} from 'moment';
 
-const moment= _moment;
-
-// See the Moment.js docs for the meaning of these formats:
-// https://momentjs.com/docs/#/displaying/format/
-export const MY_FORMATS = {
-  parse: {
-    dateInput: 'LL',
-  },
-  display: {
-    dateInput: 'LL',
-    monthYearLabel: 'MMM-YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMM-DD-YYYY',
-  },
-};
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css'],
-  providers: [
-    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
-    // application's root module. We provide it at the component level here, due to limitations of
-    // our example generation script.
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-
-    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
-  ],
 })
 
 export class SearchResultsComponent implements OnInit {
@@ -55,8 +24,16 @@ export class SearchResultsComponent implements OnInit {
   nyc : boolean=false; 
   paris : boolean=false;
   panelOpenState: boolean;
-  date = new FormControl(moment());
-
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
+  crime : boolean = false;
+  environment : boolean = false; 
+  politics : boolean=false; 
+  sUnrest : boolean = false; 
+  infra : boolean =false;
+  verified : boolean=false; 
+  regular : boolean = false; 
+  
 
   @Input() inputQuery : string;
   @Input() apiResponse : any;
@@ -79,7 +56,7 @@ export class SearchResultsComponent implements OnInit {
     this.pageIndex = event.pageIndex;
     this.filtersChanged();
   }
-
+  
   public filtersChanged(){
     var filter : any[];
     let langFilter : string = '';
@@ -91,25 +68,31 @@ export class SearchResultsComponent implements OnInit {
     langFilter=langFilter.substring(0,(langFilter.length)-1)+'';
     if(langFilter=='"') langFilter='""';
     
-    let cityFilter : string = '"';
+    let cityFilter : string = '';
     if(this.bangkok==true)  cityFilter+='"bangkok",';
     if(this.delhi==true)  cityFilter+='"delhi",';
     if(this.mexico_city==true)  cityFilter+='"mexico city",';
     if(this.nyc==true)  cityFilter+='"nyc",';
     if(this.paris==true)  cityFilter+='"paris",';
-    cityFilter=cityFilter.substring(0,(cityFilter.length)-1)+'"';
+    cityFilter=cityFilter.substring(0,(cityFilter.length)-1)+'';
     if(cityFilter=='"') cityFilter='""';
+
+    let topicFilter : string = '';
+    if(this.crime==true)  topicFilter+='"crime",';
+    if(this.politics==true)  topicFilter+='"politics",';
+    if(this.environment==true)  topicFilter+='"environment",';
+    if(this.nyc==true)  topicFilter+='"nyc",';
+    if(this.paris==true)  topicFilter+='"paris",';
+    topicFilter=topicFilter.substring(0,(topicFilter.length)-1)+'';
+    if(topicFilter=='"') topicFilter='""';
 
     let pageNo = this.pageIndex;
     let resultsPerPage = this.pageSize;
 
     filter=[langFilter, cityFilter, pageNo, resultsPerPage];
     this.filtersEmitter.emit(filter);
-   
-   
-    let  dates:string=this.date.value._d;
-   // dates=dates.substring(5,15).toUpperCase().replace(' ','_');
-    console.log(dates);
+    console.log(this.serializedDate);
+    console.log("heree");
 
   }
 
