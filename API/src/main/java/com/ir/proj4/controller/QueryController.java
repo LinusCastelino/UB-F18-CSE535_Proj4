0042765da9bd14352414a52bd68abae9ed4b50f0
91Vallabh <ramvalla@buffalo.edu>
2018-12-02 23:11:30 -0500

    removed hashtags from solrservice and included it in solrstatservice

--- API/src/main/java/com/ir/proj4/controller/QueryController.java
+++ API/src/main/java/com/ir/proj4/controller/QueryController.java
@@ -37,11 +37,11 @@ public class QueryController<JSONObject> {
 
 	@CrossOrigin
 	@RequestMapping(value="statistics", method = RequestMethod.GET)
-	public ReturnStatisticsList getSolrStatistics(@RequestParam(name="q") String query, @RequestParam(name="date", required=false) String date, @RequestParam(name="pageSize") String pageSize, @RequestParam(name="pageNo") String pageNo, @RequestParam(name="lang", required=false) String lang, @RequestParam(name="topic", required=false) String topic, @RequestParam(name="verified", required=false) String verified, @RequestParam(name="city", required=false) String city) throws URISyntaxException, GeneralSecurityException, IOException {
+	public ReturnStatisticsList getSolrStatistics(@RequestParam(name="q") String query, @RequestParam(name="lang", required=false) String lang, @RequestParam(name="topic", required=false) String topic, @RequestParam(name="verified", required=false) String verified, @RequestParam(name="city", required=false) String city) throws URISyntaxException, GeneralSecurityException, IOException {
 		//to work on
 		//most common ISO Date Format yyyy-MM-dd
 		
-		return solrStatistics.querySolr(query, date, pageSize, pageNo, lang, topic, verified, city);
+		return solrStatistics.querySolr(query, lang, topic, verified, city);
 
 
 	}
