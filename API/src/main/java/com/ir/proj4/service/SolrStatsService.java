87f3d0f09a023c1b38e3a426f363422df50495d6
Harsh Gandhi <harshnar@buffalo.edu>
2018-12-02 23:57:12 -0500

    ram's error resolved

--- API/src/main/java/com/ir/proj4/service/SolrStatsService.java
+++ API/src/main/java/com/ir/proj4/service/SolrStatsService.java
@@ -23,24 +23,24 @@ import com.ir.proj4.model.ReturnStatisticsList;
 public class SolrStatsService {
 
 	public ReturnStatisticsList querySolr(String query, String lang,String topic,String verified, String city) throws URISyntaxException, GeneralSecurityException, IOException {
-		
+		SentimentAnalysis.init();
 	
 		String q3;
 		String url;
 		
 		
 		//some preprocessing on query params
-		if(lang==null || lang.equals("") || lang.equals("\"\""))
-			lang="\"en\",\"es\",\"hi\",\"th\",\"fr\"";
-		if(city==null || city.equals("") || city.equals("\"\"") )
-			city="\"mexico%20city\",\"paris\",\"bangkok\",\"delhi\",\"nyc\"";
-		else
-			city=city.replace(" ", "%20");
-		
-		System.out.println("verified is : "+ verified);
-		if(verified==null || verified.equals("") || verified.equals("false"))
-			verified="\"true\"%7C%7C\"false\"";
-		
+//		if(lang==null || lang.equals("") || lang.equals("\"\""))
+//			lang="\"en\",\"es\",\"hi\",\"th\",\"fr\"";
+//		if(city==null || city.equals("") || city.equals("\"\"") )
+//			city="\"mexico%20city\",\"paris\",\"bangkok\",\"delhi\",\"nyc\"";
+//		else
+//			city=city.replace(" ", "%20");
+//		
+//		System.out.println("verified is : "+ verified);
+//		if(verified==null || verified.equals("") || verified.equals("false"))
+//			verified="\"true\"%7C%7C\"false\"";
+//		
 		
 	
 		
@@ -77,15 +77,16 @@ public class SolrStatsService {
         ObjectMapper obj_ObjectMapper = new ObjectMapper();
         QueryData obj_QueryData = new QueryData();
         obj_QueryData = obj_ObjectMapper.readValue(response.toString(), QueryData.class);
-//        for(Docs doc : obj_QueryData.getResponse().getDocs() ) {
-////        	doc.setSemanticScore(sentimentAnalysis(doc.getText().get(0))); 
+        for(Docs doc : obj_QueryData.getResponse().getDocs() ) {
+//        	doc.setSemanticScore(sentimentAnalysis(doc.getText().get(0))); 
+        	doc.setSentiment(SentimentAnalysis.findSentiment(doc.getText().get(0)));
 //        	List<String> temp= new ArrayList<String>();
 //        	temp.add(doc.getTopic().get(0).substring(0, 1).toUpperCase() +doc.getTopic().get(0).substring(1));
 //        	doc.setTopic(temp);
 //        	temp = new ArrayList<String>();
 //        	temp.add(doc.getCity().get(0).substring(0, 1).toUpperCase() +doc.getCity().get(0).substring(1));
 //        	doc.setCity(temp);
-//        }
+        }
 //        ReturnList returnList = new ReturnList(obj_QueryData.getResponse().getDocs(),obj_QueryData.getFacet_counts().getFacet_fields().getLang(),obj_QueryData.getFacet_counts().getFacet_fields().getCity(),obj_QueryData.getResponse().getNumFound(),obj_QueryData.getFacet_counts().getFacet_fields().getTopic());
         ReturnStatisticsList returnStatList = new ReturnStatisticsList(obj_QueryData);
        //System.out.println(returnList.getTweets().get(0).getText());
